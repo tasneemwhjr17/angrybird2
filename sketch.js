ca8fba0978a02f06f4214c198f6167e48dfa5f86
tasneem26 <tasneemwhjr17@gmail.com>
2021-04-26 16:26:41 +0530

    Added Platform

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg, 
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +15,6 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -46,6 +45,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+   
     pig1.display();
     log1.display();
 
